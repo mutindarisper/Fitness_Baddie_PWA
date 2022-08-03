5591766c4a9b2b6bf62a8252c338965993e779d3
unknown <mutindarisper@gmail.com>
2022-08-03 11:23:21 +0300

    changed api key

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -3,7 +3,7 @@ export const exerciseOptions = {
     // url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
     headers: {
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY_2
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
       
     }
   };
@@ -12,7 +12,7 @@ export const exerciseOptions = {
   export const youtubeOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key':  process.env.REACT_APP_RAPID_API_KEY_2,
+      'X-RapidAPI-Key':  process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
   };
